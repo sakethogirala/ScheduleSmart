d4af79dba2335ce3ccc31bb438ceadf40814e58c
sogirala <sogirala@umich.edu>
2024-06-11 20:25:13 -0400

    Initial Commit

---
+++ ScheduleSmart/ScheduleSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleSmartApp.swift
+//  ScheduleSmart
+//
+//  Created by Saketh Ogirala on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
